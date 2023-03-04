8a8509816067aea32e0f2250c099a4b8e31ab551
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-03-04 03:02:15 +0000

    [Finish]ruby

--- lesson11.rb
+++ lesson11.rb
@@ -1,13 +1,13 @@
 class Car
   def run(distance)
-   puts "車で#{distance}キロ走ります。"
+    puts "車で#{distance}キロ走ります。"
   end
 end
 
 class Truck < Car
   def run(distance)
     super
-    puts "大きな荷物を乗せて走ります。"
+    puts "大きな荷物を乗せて走ります"
   end
 end
 

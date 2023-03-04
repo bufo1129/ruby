038d6fa8fc499d40d6e5b93a1876d566d26b183c
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-03-04 01:50:34 +0000

    [Add]ruby]

--- lesson10.rb
+++ lesson10.rb
@@ -1,13 +1,13 @@
 class Car
   def run(distance)
-    puts "車で#{distance}走ります。"
+    puts "車で#{distance}キロ走ります。"
   end
 end
 
 class Taxi < Car
 end
 
-taxi = Taxi.new
-taxi.run(5)
+car = Taxi.new
+car.run(5)
 
 puts Taxi.superclass
